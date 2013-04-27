dd65fb255d387f9c7d8c1f539cad6e673b2fe5ae
Schuyler Martin <schuylermartin45@gmail.com>
2013-04-27 19:12:19 -0400

    First commit
    Enjoy the glorious code, Derek

---
+++ PlanA/PlanA/PlanA/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanA
+{
+    /// <summary>
+    /// Represents a single note on the track
+    /// </summary>
+    public class Note
+    {
+        //buffer, in milliseconds on 
+        public static const int TIMEBUFFER = 500;
+        //enumeration that represents which button to associate with this note
+        public enum BUTTONS {GREEEN, RED, YELLOW, BLUE, ORANGE};
+
+        //time where the note should "start", in milliseconds
+        public int timeStart;
+        //which button is associated with this
+        public BUTTONS button;
+
+        public Note(int timeStart, BUTTONS button)
+        {
+            this.
+            this.button = button;
+        }
+    }
+}
---
+++ PlanA/PlanB/PlanB.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PlanB
+{
+    public partial class PlanB : Form
+    {
+        public PlanB()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.PARKOUR);
+            simpleSound.Play();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.PARKOUR_YEAH);
+            simpleSound.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.Wee);
+            simpleSound.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.YEAH_LOUDER);
+            simpleSound.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.SAVED);
+            simpleSound.Play();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.BIBLESPLOSION);
+            simpleSound.Play();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.HOLYWARsound);
+            simpleSound.Play();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.CONDEMNED);
+            simpleSound.Play();
+        }
+    }
+}
